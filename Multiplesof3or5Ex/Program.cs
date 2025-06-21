3fdcd8610651c94ea52b558b7fd934dda57d76b3
Gaël Louage <gaellouage@hotmail.com>
2025-06-21 22:23:51 +0200

    mulitples of 3 or 5 exercise

---
+++ Multiplesof3or5Ex/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Solution(10));
+    }
+
+    public static int Solution(int value)
+    {
+        var result = 0;
+        for (int i = 0; i < value; i++) 
+        {
+            if(i%3 == 0 || i %5  == 0)
+            {
+                result += i;
+            }
+        }
+        return result;
+    }
+}
